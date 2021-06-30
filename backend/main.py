5bc73cda37ae3460ef4b87e0e51d01f870b33254
tomoya shibazaki <tomoyashibazaki@tomoya-shibazaki.local>
2021-07-01 05:24:59 +0900

    add pydantic

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from db import session
 from models import Users
+from schemas import User
+from typing import List
 
 app = FastAPI()
 
@@ -9,7 +11,7 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/users")
+@app.get("/users", response_model=List[User])
 def read_users():
     users = session.query(Users).all()
     return users
---
+++ backend/schemas.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    id: int
+    name: str
+    email: str
+
+    class Config:
+        orm_mode = True
