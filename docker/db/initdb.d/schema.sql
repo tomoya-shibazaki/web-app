3e5dffbdb7868f13a577ad7f356a2423ac8b75c6
tomoya shibazaki <tshibazaki8165@gmail.com>
2021-07-03 02:45:43 +0900

    update initdb

--- docker/db/initdb.d/schema.sql
+++ docker/db/initdb.d/schema.sql
@@ -1,6 +1,47 @@
+-- ---
+-- Table 'users'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255),
-    PRIMARY KEY (id)
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+-- ---
+-- Table 'courses'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS courses;
+		
+CREATE TABLE courses (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+-- ---
+-- Table 'users_courses'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS users_courses;
+		
+CREATE TABLE users_courses (
+  id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE,
+  course_id INT NOT NULL,
+  FOREIGN KEY (course_id)
+    REFERENCES courses(id)
+    ON DELETE CASCADE,
+  status ENUM('todo', 'wip', 'done') DEFAULT 'todo',
+  PRIMARY KEY (id)
 );
--- docker/db/initdb.d/testdata.sql
+++ docker/db/initdb.d/testdata.sql
@@ -1,1 +1,20 @@
+-- ---
+-- Insert Table 'users'
+-- 
+-- ---
 INSERT INTO users (name, email) VALUES ("admin", "admin@gmail.com");
+INSERT INTO users (name, email) VALUES ("student", "student@gmail.com");
+
+-- ---
+-- Insert Table 'courses'
+-- 
+-- ---
+
+INSERT INTO courses (name) VALUES ("programming");
+
+
+-- ---
+-- Insert Table 'courses'
+-- 
+-- ---
+INSERT INTO users_courses (user_id, course_id, status) VALUES (1, 1, "done");
