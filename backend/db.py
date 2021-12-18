063138530f8571633671040b061a1e1a4ec7a3fc
tomoya shibazaki <tshibazaki8165@gmail.com>
2021-12-18 20:31:27 +0900

    add .env file

--- backend/db.py
+++ backend/db.py
@@ -1,11 +1,27 @@
+import os
+from dotenv import load_dotenv
+from os.path import join, dirname
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-user_name = "user"
-password = "password"
-host = "db"
-database_name = "sample_db"
+load_dotenv(verbose=True)
+
+dotenv_path = join(dirname(__file__), '.env')
+print('PATH ', dotenv_path)
+load_dotenv(dotenv_path)
+USER_NAME = os.environ.get("MYSQL_USER", 'user')
+PASSWORD = os.environ.get("MYSQL_PASSWORD", 'password')
+HOST = os.environ.get("MYSQL_HOST_NAME", 'db')
+DATABASE_NAME = os.environ.get("MYSQL_DATABASE", 'sample_db')
+
+H = os.environ.get("MYSQL_HOST_NAME")
+print('H ', H) 
+
+user_name = USER_NAME
+password = PASSWORD
+host = HOST
+database_name = DATABASE_NAME
 
 DATABASE = 'mysql://%s:%s@%s/%s?charset=utf8' % (
     user_name,
